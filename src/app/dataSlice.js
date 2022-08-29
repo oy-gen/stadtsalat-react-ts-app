516d629e5f27861537ecad849edc2e9c7a3721be
Eugen Dirksen <dirksen.eugen@gmail.com>
2022-08-29 16:08:38 +0200

    clean up

--- src/app/dataSlice.js
+++ src/app/dataSlice.js
@@ -12,9 +12,7 @@ export const dataSlice = createSlice({
     addProducts: (state, action) => {
       state.products = action.payload;
     },
-    addPaginatedProducts: (state, action) => {
-      state.paginatedProducts = action.payload;
-    },
+
     toggleLike: (state, action) => {
       if (state.likes.includes(action.payload)) {
         state.likes = state.likes.filter((id) => action.payload !== id);
@@ -25,10 +23,9 @@ export const dataSlice = createSlice({
   },
 });
 
-export const { addProducts, addPaginatedProducts, toggleLike } = dataSlice.actions;
+export const { addProducts, toggleLike } = dataSlice.actions;
 
 export const showLikes = (state) => state.data.likes;
 export const showProducts = (state) => state.data.products;
-export const showPaginatedProducts = (state) => state.data.paginatedProducts;
 
 export default dataSlice.reducer;
